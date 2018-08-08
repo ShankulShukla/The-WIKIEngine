f85a3c18b3dd2d5a6aecb9b2c141ea6e390396a7
Shankul Shukla <30331393+ShankulShukla@users.noreply.github.com>
2018-08-08 07:45:12 +0530

    update

--- WIKI-search.py
+++ WIKI-search.py
@@ -1,10 +1,12 @@
 import json
 from nltk.stem import SnowballStemmer
 #including the index into the program
+print("Loading the parsed dictionary...")
 with open('data.json','r') as inp:
     index=json.load(inp)
 
 #input the keyword to be searched
+print("Search for a keyword - ")
 raw_input=input()
 t=raw_input.split(' ')
 #converting the input search into the type of value stored in the index by using stemming
@@ -19,7 +21,7 @@ print('For the keyword "{}" related wikipedia pages are-'.format(raw_input))
 ## Criteria would be to search the exact word in the index serving as keys.
 
 if actual_input in index.keys():
-    print('Main link-',end=' ')
+    print('\nMain link-',end=' ')
     for g in index[actual_input]:
         print(g)
         result.append(g)
@@ -53,3 +55,6 @@ for i in index.keys():
 #if no result is found
 if len(result) == 0:
     print('PLEASE !!! Do update the crawler for this keyword for future purposes....')
+# for running the code purpose
+print("*"*150)
+print("See the code to understand what stages mean..")
